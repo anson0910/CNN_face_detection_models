04007f7f5b209b3c28e50ca3acd3db7f40dd698c
Anson <anson32330200@gmail.com>
2016-03-25 21:01:09 +0800

    Add scripts to create lmdb files

---
+++ create_lmdb_scripts/face_12c/create_face_12c.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env sh
+# Create the face_48 lmdb inputs
+# N.B. set the path to the face_48 train + val data dirs
+
+EXAMPLE=examples/face_12c
+DATA=data/face
+TOOLS=build/tools
+
+TRAIN_DATA_ROOT=/home/anson/face_pictures/train_12c/
+VAL_DATA_ROOT=/home/anson/face_pictures/val/
+
+# Set RESIZE=true to resize the images to 60 x 60. Leave as false if images have
+# already been resized using another tool.
+RESIZE=true
+if $RESIZE; then
+  RESIZE_HEIGHT=15
+  RESIZE_WIDTH=15
+else
+  RESIZE_HEIGHT=0
+  RESIZE_WIDTH=0
+fi
+
+if [ ! -d "$TRAIN_DATA_ROOT" ]; then
+  echo "Error: TRAIN_DATA_ROOT is not a path to a directory: $TRAIN_DATA_ROOT"
+  echo "Set the TRAIN_DATA_ROOT variable in create_face_48.sh to the path" \
+       "where the face_48 training data is stored."
+  exit 1
+fi
+
+if [ ! -d "$VAL_DATA_ROOT" ]; then
+  echo "Error: VAL_DATA_ROOT is not a path to a directory: $VAL_DATA_ROOT"
+  echo "Set the VAL_DATA_ROOT variable in create_face_48.sh to the path" \
+       "where the face_48 validation data is stored."
+  exit 1
+fi
+
+echo "Creating train lmdb..."
+
+GLOG_logtostderr=1 $TOOLS/convert_imageset \
+    --resize_height=$RESIZE_HEIGHT \
+    --resize_width=$RESIZE_WIDTH \
+    --shuffle \
+    $TRAIN_DATA_ROOT \
+    $DATA/train_12c.txt \
+    $EXAMPLE/face_train_lmdb
+
+echo "Creating val lmdb..."
+
+GLOG_logtostderr=1 $TOOLS/convert_imageset \
+    --resize_height=$RESIZE_HEIGHT \
+    --resize_width=$RESIZE_WIDTH \
+    --shuffle \
+    $VAL_DATA_ROOT \
+    $DATA/val.txt \
+    $EXAMPLE/face_val_lmdb
+
+echo "Done."
